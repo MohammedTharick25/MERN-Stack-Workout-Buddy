76a6436e937c3b8e1287f6c86a4941ba4126ef09
Mohammed Tharick <mdtharick25@gmail.com>
2026-01-09 11:49:49 +0530

    last updated

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ app.use("/api/user", userRoutes);
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
   });
 }
 
